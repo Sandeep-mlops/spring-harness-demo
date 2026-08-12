28666fc470d6ff4991042348630864cd63b0342e
Sandeep guntupalli <sandeepguntupalli@Sandeeps-MacBook-Air.local>
2026-08-12 15:10:11 -0500

    Initial commit: 5 Java 17 Spring Boot Apps

---
+++ app-five/src/main/java/com/example/demo/AppFiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFiveApplication.class, args);
+	}
+
+}
---
+++ app-four/src/main/java/com/example/demo/AppFourApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFourApplication.class, args);
+	}
+
+}
